656a0cf4195a04e80bb7a8796d7911a10bbf8e3d
sugawara-riku <sugariku0303@outlook.jp>
2022-12-09 07:43:49 +0000

    fix edit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,17 +6,17 @@
     </div>
     
     <div class="flex justify-center">
-       <form method="POST" actin="{{ route('tasks.update', $task->id) }}" class="w-1/2">
+       <form method="POST" action="{{ route('tasks.update', $task->id) }}" class="w-1/2">
            @csrf
            @method('PUT')
         
-                <div class="form-control my-4">
-                    <label for="content" class="label">
-                        <span class="label-text">タスク:</span>
-                    </label>
-                    <input type="text" name="content" value="{{ $task->content }}" class="input input-bordered w-full">
-                </div>
-            <button type="submit" class="btn btn-primary btn-outline">更新</button>
+               <div class="form-control my-4">
+                   <label for="content" class="label">
+                       <span class="label-text">タスク:</span>
+                   </label>
+                   <input type="text" name="content" value="{{ $task->content }}" class="input input-bordered w-full">
+               </div>
+           <button type="submit" class="btn btn-primary btn-outline">更新</button>
        </form> 
     </div>
 @endsection
