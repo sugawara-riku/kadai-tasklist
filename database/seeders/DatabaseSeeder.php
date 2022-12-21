50543e6d8c0a13488724fde34cfe68ec660e0aaf
sugawara-riku <sugariku0303@outlook.jp>
2022-12-21 23:16:08 +0000

    laravel breeze daisyui

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
